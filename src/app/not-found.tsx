c4e617cd5e25cc47085c00034ed8f9af91062857
Foysal Ahmed <foysalxahmed@gmail.com>
2024-11-01 12:48:10 +0600

    Update not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,7 +1,7 @@
 const NotFoundPage = () => {
   return (
     <div className='flex h-screen items-center justify-center'>
-      <h1 className='font-mono text-4xl'>
+      <h1 className='font-mono text-4xl font-semibold'>
         <span className='xl:2xl mr-2.5 block text-center font-mono text-xl text-primary'>
           404.
         </span>{' '}
